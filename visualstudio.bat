88672ef7c8a550cf5f705f2e9fd1bb7dd51efbc5
sebastian reitzig <89968431+sebbe-mvt@users.noreply.github.com>
2022-03-08 15:00:48 +0100

    bat fil för att köra test

---
+++ visualstudio.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" test1/test1/bin/Debug/net6.0/test1.dll 
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" test1/test2/test2/bin/Debug/net6.0/test2.dll
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" test1/test3/test3/bin/Debug/net6.0/test3.dll
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" test1/test4/bin/Debug/net6.0/test4.dll
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" test1/test5/bin/Debug/net6.0/test5.dll
